0709e2d2ad4df3d1783a6aa13d445fb31ce8e1f6
oguzergul <erguloguz@hotmail.com>
2021-10-22 06:54:01 +0300

    [add]: models has been created.

---
+++ app/models/analytics.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    files: {
+        type: Array,
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    measurement_date: {
+        type: Date,
+        default: Date.now
+    },
+    user_agent: {
+        type: String,
+        required: true
+    },
+    ttfbTime: {
+        type: Number,
+        required: true
+    },
+    fcpTime: {
+        type: Number,
+        required: true
+    },
+    domLoadTime: {
+        type: Number,
+        required: true
+    },
+    windowLoadTime: {
+        type: Number,
+        required: true
+    },
+    resourcesLoadTime: {
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
