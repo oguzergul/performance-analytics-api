732093131cbb5dee9ddeb33a008b93d029ef03cc
oguzergul <erguloguz@hotmail.com>
2021-10-23 12:28:31 +0300

    refactor: performance api refactored.

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 const db = mongoose.connection;
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
@@ -16,7 +15,6 @@ app.use(cors());
 db.once("open", () => {
     require("./routes/analytics.routes")(app)
 })
-
 
 app.listen(PORT, async () => {
     console.log(`Server is working on ${PORT}. Database connection process started`);
