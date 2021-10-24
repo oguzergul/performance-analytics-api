c0759b8c2a73de728dc2f613c6f5cde83f6950d0
oguzergul <you@example.com>
2021-10-24 03:31:35 +0300

    [feat]: database connection has been refactored. configs and loaders initialized.

---
+++ loaders/db.js
@@ -0,0 +1,16 @@
+const Mongoose = require('mongoose');
+
+Mongoose.connection.once("open", () => {
+    console.log("Mongoose DB Connection is Successful!")
+})
+
+const connectDB = async () => {
+    await Mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DATABASE}.uvc5x.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+
+module.exports = {
+    connectDB
+}
