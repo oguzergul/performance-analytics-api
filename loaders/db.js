86ee5b74064fe5b5755fe8765e5b38b7459aecf4
oguzergul <erguloguz@hotmail.com>
2021-10-25 04:17:10 +0300

    [feat]: api refactored. tests created.

--- loaders/db.js
+++ loaders/db.js
@@ -2,12 +2,18 @@ const Mongoose = require('mongoose');
 
 Mongoose.connection.once("open", () => {
     console.log("Mongoose DB Connection is Successful!")
-})
+});
+
+Mongoose.connection.once("error", () => {
+    console.log("MongoDB Connection is Failed!")
+});
 
 const connectDB = async () => {
     await Mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DATABASE}.uvc5x.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
+    }).catch(err => {
+        console.log(err);
     });
 }
 
--- test/performance.test.js
+++ test/performance.test.js
@@ -1,11 +1,10 @@
 const request = require("supertest");
 const mongoose = require('mongoose');
-const dbConfig = require('../config/database.config');
 
 const PORT = process.env.PORT || "http://localhost:3000";
 
 beforeEach((done) => {
-    mongoose.connect(dbConfig.url,
+    mongoose.connect(PORT,
         {useNewUrlParser: true, useUnifiedTopology: true}, () => done());
 });
 
@@ -26,36 +25,46 @@ const mockReport = {
 }
 
 describe('Analytics API', () => {
+
     it('should get all data from api', async () => {
         const response = await request(PORT).get('/performance');
-        expect(response.status).toEqual(200)
-        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response?.status).toEqual(200);
+        expect(Array.isArray(response?.body)).toBeTruthy();
     });
 
     it('should return last 30 minutes data', async () => {
         const response = await request(PORT).get('/');
-        const data = response.body.slice(-1)[0].measurement_date;
+        const data = response?.body?.slice(-1)[0]?.measurement_date;
         const minDate = new Date(new Date().getTime() - 1000 * 60 * 30);
-        expect(new Date(data) > new Date(minDate)).toEqual(true);
+
+        if (response.body.length) {
+            expect(new Date(data) > new Date(minDate)).toEqual(true);
+        }
+        expect(Array.isArray(response?.body)).toBeTruthy()
+
     });
 
     it('should get specific dates data from api', async () => {
-        const response = await request(PORT).get(`/performance/?min=2021-10-01T17:31&max=2021-10-23T17:31`);
+        const minDate = "2021-10-01T17:31";
+        const maxDate = "2021-10-23T17:31";
+        const response = await request(PORT).get(`/performance/?min=${minDate}&max=${maxDate}`);
+
         expect(response.status).toEqual(200);
-        expect(Array.isArray(response.body)).toBeTruthy()
+        expect(Array.isArray(response.body)).toBeTruthy();
     });
 
     it('should create new performance record', async () => {
-        const response = await request(PORT).post('/analytics').send(mockReport);
-        expect(response.status).toEqual(200);
+        const response = await request(PORT).post('/performance').send(mockReport);
+        console.log('Response', response);
+        expect(response.status).toEqual(201);
     });
 
     it('should send status:500 when invalid req', async () => {
-        const response = await request(PORT).post('/analytics').send({
+        const response = await request(PORT).post('/performance').send({
             one: 'first dummy data',
             two: 'second dummy data'
         });
-        expect(response.status).toEqual(400);
+        expect(response.status).toEqual(500);
     });
 
 })
