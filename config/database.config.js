6b34a0533b5e8813866bfafb7027fd49244a157a
oguzergul <erguloguz@hotmail.com>
2021-10-25 06:06:13 +0300

    [config]: configurations has been refactored

--- config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'mongodb+srv://oguz_ergul:OGuz98.5ouz@performance-reporter.uvc5x.mongodb.net/performance-reporter?retryWrites=true&w=majority'
-}
--- loaders/db.js
+++ loaders/db.js
@@ -12,9 +12,7 @@ const connectDB = async () => {
     await Mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_DATABASE}.uvc5x.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }).catch(err => {
-        console.log(err);
-    });
+    })
 }
 
 module.exports = {
